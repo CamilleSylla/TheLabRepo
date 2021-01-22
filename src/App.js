0fb4c749397cc5ff3ea499be622d9328b6c5d150
Camille Sylla <camillesylla.web@gmail.com>
2021-01-22 03:05:33 +0100

    added Glitch Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import WavyText from './Component/WavyText/WavyText';
 import OpenYourEyes from './Component/OpenYourEyes/OpenYourEyes';
+import Glicth from './Component/Glicth/Glitch';
 
 function App() {
   return (
     <div className="App">
-      <OpenYourEyes/>
+      <Glicth/>
     </div>
   );
 }
---
+++ src/Component/Glicth/Glitch.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import "../../Styles/Glicth.scss";
+
+export default function Glicth() {
+  const Glitch = "Glitch";
+
+ 
+
+  function Multiply() {
+    const Arr = [];
+    for (let i = 0; i <= 3; i++) {
+      Arr.push(Glitch);
+    }
+    return Arr.map((Glitch, i) => {
+      return (
+        <p className="glitch_text" key={i} >
+          {Glitch}
+        </p>
+      );
+    });
+  }
+
+
+  return <div id="glicth">{Multiply()}</div>;
+}
+
+
